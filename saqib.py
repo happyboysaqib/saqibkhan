58be061c6ae6cf5a8c189017df04817e3b538352
happyboysaqib <65262872+happyboysaqib@users.noreply.github.com>
2022-07-29 17:34:06 +0400

    Create saqib.py
     pkg update
     pkg upgrade
     pkg install python
     pkg install git
     pip install requests
     pip install bs4
     pip install futures
     pip install mechanize
     cd $HOME rm -rf SS
     git clone https://github.com/Sqib-khan/king
    cd saqib
     python Saqib.py

---
+++ saqib.py
@@ -0,0 +1,112 @@
+____Programming And Gaming____
+
+CONTACT WITH <a href="https://github.com/Sarfraz-Ssb"><b>Saqib.khan </a> </br><br>
+
+<img src="https://github.com/Saqib-khan/Saqib-Ssb/blob/main/IMAGE/contact.png" alt="alt text" width="25" height="25"> <br>
+
+<p>saqiburrehman048@gmail.com</p>  <br> <br> 
+
+<a href="https://github.com/Saqib-khan/followers">
+
+<img title="Followers" src="https://img.shields.io/github/followers/Saqib-khan?label=Followers&color=blue&style=flat-square"></a>
+
+<br>
+
+  <a href="https://github.com/Saqib-khan/termux-style/stargazers/">
+
+  <a href="https://github.com/Saqib-khan/king">
+
+    <img alt="Last Commit" src="https://img.shields.io/github/last-commit/Sqib-khan/king.svg"/>
+
+  </a>
+
+<br>
+
+  <a href="https://github.com/Saqib-khan/king">
+
+    <img alt="Language" src="https://img.shields.io/github/languages/count/Saqib-khan/king.svg"/>
+
+  </a>
+
+  <a href="https://github.com/Saqib-khan/king">
+
+    <img alt="Starts" src="https://img.shields.io/github/stars/Saqib-khan/king.svg"/>
+
+  </a>
+
+<br>
+
+<a href="https://github.com/Saqib-khan/king">
+
+    <img alt="Repo Size" src="https://img.shields.io/github/repo-size/Saqib-king/khan.svg"/>
+
+  </a>
+
+<br>
+
+<a href="https://github.com/Saqib-khan/king">
+
+    <img alt="Top Language" src="https://img.shields.io/github/languages/top/Saqib-khan/king.svg"/> <a                                                                                                        href="https://github.com/Azim-vau/fcpromax">
+
+    <img alt="Forks" src="https://img.shields.io/github/forks/Saqib-khan/king.svg"/>
+
+  </a>
+
+</div>
+
+</br>
+
+<p align="center">
+
+      FACEBOOK ACCOUNT CLONER
+
+</p>
+
+  
+
+#### INSTALL TOOL ON TERMUX
+
+```python
+
+ pkg update
+
+ pkg upgrade
+
+ pkg install python
+
+ pkg install git
+
+ pip install requests
+
+ pip install bs4
+
+ pip install futures
+
+ pip install mechanize
+
+ cd $HOME 
+
+ rm -rf SS
+
+ git clone https://github.com/Sqib-khan/king
+
+```
+
+#### RUN SCRIPT
+
+```python
+
+ cd saqib
+
+ python Saqib.py
+
+```
+
+#### MY SOCIAL MEDIA
+
+[![](https://img.shields.io/badge/Github-black?logo=Github&logoColor=red&labelColor=black)](https://github.com/Saqiburrehman) <br>
+
+[![](https://img.shields.io/badge/Facebook-black?logo=Facebook&logoColor=red&labelColor=black)](https://www.fakebook.com/msb.Father.of.fb) <br>
+
+<h2> whatsapp 03484160325 <h2\>
+
